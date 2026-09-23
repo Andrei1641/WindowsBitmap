a220865b7a6989ae707d1a6ac2d7df21f867906f
Andrei <andrijskorofatov111@gmail.com>
2026-09-23 19:22:37 +0200

    tools

--- tools.py
+++ tools.py
@@ -1,13 +1,24 @@
+import math
+
 class Translator:
     @staticmethod
-    def from_int_to_byte(integer: int) -> list[bytes]:
-        ...
+    def from_int_to_bytes(number: int) -> list[int]:
+        number_of_bits: int = Translator.__get_number_of_bits_in_int(number)
+        number_of_bytes: int = math.ceil(number_of_bits / 8)
+        return list(number.to_bytes(number_of_bytes))
+
     @staticmethod
-    def translate_endian(b: list[bytes]) -> list[bytes]:
-        ...
+    def __get_number_of_bits_in_int(number: int) -> int:
+        return math.ceil(math.log(number, 2))
+
+    @staticmethod
+    def translate_endian(b: list[int]) -> list[int]:
+        return b[::-1]
 
 
 class CreationProver:
     @staticmethod
     def is_out_of_border(byte_number_border: int, number: int) -> bool:
-        ...
+        if number < 2 ** (8 * byte_number_border):
+            return True
+        return False
